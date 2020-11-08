4e732734584107116f07925cf63ea1ac5d808adb
Vidya <Vidya@DESKTOP-AI4H7LF>
2020-11-08 14:12:41 +0530

    First Commit to GIT

---
+++ Demo1/src/DemoExJava/PractoHelloWorldEx.java
@@ -0,0 +1,28 @@
+package DemoExJava;
+
+public class PractoHelloWorldEx {
+
+	public static void main(String[] args) {
+		
+//		int i=0;
+//		int m=i++;
+//		System.out.println(m);
+//		System.out.println(i);
+//		
+//		int j=0;
+//		int n=j+1;
+//		System.out.println(n);
+//		System.out.println(j);
+		
+		int a=0;
+		int x=++a;
+		System.out.println(a);
+		System.out.println(x);
+		
+		
+		
+			
+
+	}
+
+}
---
+++ Demo1/src/DemoExJava/PractoVarWorldEx.java
@@ -0,0 +1,11 @@
+package DemoExJava;
+
+public class PractoVarWorldEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
